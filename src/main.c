7c813b37f7721a68d29b9fc86344c907ae57ecad
Guillem Arias Fauste <hello@gariasf.com>
2024-07-16 16:03:30 +0200

    full screen video mode

--- src/main.c
+++ src/main.c
@@ -21,6 +21,12 @@ bool initialize_window(void)
         return false;
     }
 
+    SDL_DisplayMode display_mode;
+    SDL_GetCurrentDisplayMode(0, &display_mode);
+
+    window_width = display_mode.w;
+    window_height = display_mode.h;
+
     window = SDL_CreateWindow(
         NULL,
         SDL_WINDOWPOS_CENTERED,
@@ -42,6 +48,8 @@ bool initialize_window(void)
         fprintf(stderr, "Error creating SDL renderer. \n");
         return false;
     }
+
+    SDL_SetWindowFullscreen(window, SDL_WINDOW_FULLSCREEN);
 
 
     return true;
