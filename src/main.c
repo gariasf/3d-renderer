bceb628164c0b98db63c9cde7ad782ecff72c201
Guillem Arias Fauste <hello@gariasf.com>
2024-07-16 17:39:39 +0200

    project simple 3d data structure

--- src/main.c
+++ src/main.c
@@ -5,17 +5,40 @@
 #include "display.h"
 #include "vector.h"
 
+#define N_POINTS (9 * 9 * 9)
+
+vec3_t cube_points[N_POINTS];
+vec2_t projected_points[N_POINTS];
+
+float fov_factor = 128;
+
 bool is_running = false;
 
-void setup() {
+void setup()
+{
     color_buffer = (uint32_t *)malloc(sizeof(uint32_t) * window_width * window_height);
+
     color_buffer_texture = SDL_CreateTexture(
         renderer,
         SDL_PIXELFORMAT_ARGB8888,
         SDL_TEXTUREACCESS_STREAMING,
         window_width,
-        window_height
-    );
+        window_height);
+
+        int point_count = 0;
+
+
+    for (float x = -1; x <= 1; x += 0.25)
+    {
+        for (float y = -1; y <= 1; y += 0.25)
+        {
+            for (float z = -1; z <= 1; z += 0.25)
+            {
+                vec3_t point = { .x = x, .y = y, .z = z };
+                cube_points[point_count++] = point;
+            }
+        }
+    }
 }
 
 void process_input(void)
@@ -29,26 +52,49 @@ void process_input(void)
         is_running = false;
         break;
     case SDL_KEYDOWN:
-        if(event.key.keysym.sym == SDLK_ESCAPE) {
+        if (event.key.keysym.sym == SDLK_ESCAPE)
+        {
             is_running = false;
         }
         break;
     }
 }
 
+vec2_t project(vec3_t point){
+    vec2_t projected_point = {
+        .x = (fov_factor * point.x),
+        .y = (fov_factor * point.y)
+    };
+
+    return projected_point;
+}
+
 void update(void)
 {
+    for (int i = 0; i< N_POINTS; i++) {
+        vec3_t point = cube_points[i];
+
+        vec2_t projected_point = project(point);
+
+        projected_points[i] = projected_point;
+    }
 }
 
 void render(void)
 {
-    SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
-    SDL_RenderClear(renderer);
-
     draw_grid();
 
-    draw_pixel(20, 20, 0xFFFFFF00);
-    draw_rect(300, 200, 300, 150, 0xFFF00FF);
+    for(int i = 0; i < N_POINTS; i++) {
+        vec2_t projected_point = projected_points[i];
+
+        draw_rect(
+            projected_point.x + window_width / 2,
+            projected_point.y + window_height / 2,
+            4,
+            4,
+            0xFFFFFF00
+        );
+    }
 
     render_color_buffer();
     clear_color_buffer(0xFF000000);
