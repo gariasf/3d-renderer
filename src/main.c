0a5d027d98a8b99abdd44bfbb7ceadc0e01b126c
Guillem Arias Fauste <hello@gariasf.com>
2024-07-16 15:52:36 +0200

    render color buffer to window surface

--- src/main.c
+++ src/main.c
@@ -1,10 +1,17 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include <stdint.h>
 #include <SDL2/SDL.h>
 
 bool is_running = false;
 SDL_Window *window;
 SDL_Renderer* renderer;
+
+uint32_t* color_buffer = NULL;
+SDL_Texture* color_buffer_texture = NULL;
+
+int window_width = 800;
+int window_height = 600;
 
 bool initialize_window(void)
 {
@@ -18,8 +25,8 @@ bool initialize_window(void)
         NULL,
         SDL_WINDOWPOS_CENTERED,
         SDL_WINDOWPOS_CENTERED,
-        800,
-        600,
+        window_width,
+        window_height,
         SDL_WINDOW_BORDERLESS);
 
     if (!window)
@@ -41,7 +48,14 @@ bool initialize_window(void)
 }
 
 void setup() {
-
+    color_buffer = (uint32_t *)malloc(sizeof(uint32_t) * window_width * window_height);
+    color_buffer_texture = SDL_CreateTexture(
+        renderer,
+        SDL_PIXELFORMAT_ARGB8888,
+        SDL_TEXTUREACCESS_STREAMING,
+        window_width,
+        window_height
+    );
 }
 
 void process_input(void)
@@ -66,12 +80,42 @@ void update(void)
 {
 }
 
+void render_color_buffer(void)
+{
+    SDL_UpdateTexture(
+        color_buffer_texture,
+        NULL,
+        color_buffer,
+        (int)(window_width * sizeof(uint32_t))
+    );
+    SDL_RenderCopy(renderer, color_buffer_texture, NULL, NULL);
+}
+
+void clear_color_buffer(uint32_t color) {
+    for(int y = 0; y < window_height; y++) {
+        for (int x = 0; x < window_width; x++)
+        {
+            color_buffer[(window_width * y) + x] = color;
+        }
+    }
+}
+
 void render(void)
 {
     SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
     SDL_RenderClear(renderer);
 
+    render_color_buffer();
+    clear_color_buffer(0xFFFFFF00);
+
     SDL_RenderPresent(renderer);
+}
+
+void destroy_window(void) {
+    free(color_buffer);
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
 }
 
 int main(void)
@@ -86,6 +130,8 @@ int main(void)
         update();
         render();
     }
+
+    destroy_window();
 
     return 0;
 }
