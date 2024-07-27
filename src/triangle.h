aa43741444f988e75d37f26abc4fa0538bdc36ae
Guillem Arias <hello@gariasf.com>
2024-07-27 12:29:56 +0200

    triangle shade WIP

--- src/triangle.h
+++ src/triangle.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <stdint.h>
 #include "vector.h"
 
 typedef struct {
@@ -11,3 +12,5 @@ typedef struct {
 typedef struct {
     vec2_t vertices[3];
 } triangle_t;
+
+void draw_filled_triangle(int x0, int y0, int x1, int y1, int x2, int y2, uint32_t color);
