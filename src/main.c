527e0254d7e3a8d4c5e52346e4fa949579fbfa4c
Guillem Arias <hello@gariasf.com>
2024-07-29 17:59:41 +0200

    implement scale transforms

--- src/main.c
+++ src/main.c
@@ -6,6 +6,7 @@
 #include "vector.h"
 #include "mesh.h"
 #include "array.h"
+#include "matrix.h"
 
 enum cull_method {
     CULL_NONE,
@@ -103,6 +104,10 @@ void update(void)
     mesh.rotation.y += 0.01;
     mesh.rotation.z += 0.01;
 
+    mesh.scale.x += 0.002;
+
+    mat4_t scale_matrix = mat4_make_scale(mesh.scale.x, mesh.scale.y, mesh.scale.y);
+
     int n_faces = array_length(mesh.faces);
     for (int i = 0; i < n_faces; i++)
     {
@@ -114,14 +119,14 @@ void update(void)
         face_vertices[2] = mesh.vertices[mesh_face.c - 1];
 
 
-        vec3_t transformed_vertices[3];
+        vec4_t transformed_vertices[3];
 
         // Loop over the vertices of the face
         for (int j = 0; j < 3; j++)
         {
-            vec3_t transformed_vertex = face_vertices[j];
+            vec4_t transformed_vertex = vec4_from_vec3(face_vertices[j]);
 
-
+            transformed_vertex = mat4_mul_vec4(scale_matrix, transformed_vertex);
 
             // Translate points away from the camera
             transformed_vertex.z += 5;
@@ -131,9 +136,9 @@ void update(void)
 
         // Backface culling test to see if the current face should be projected
         if (cull_method == CULL_BACKFACE) {
-            vec3_t vector_a = transformed_vertices[0]; /*   A   */
-            vec3_t vector_b = transformed_vertices[1]; /*  / \  */
-            vec3_t vector_c = transformed_vertices[2]; /* C---B */
+            vec3_t vector_a = vec3_from_vec4(transformed_vertices[0]); /*   A   */
+            vec3_t vector_b = vec3_from_vec4(transformed_vertices[1]); /*  / \  */
+            vec3_t vector_c = vec3_from_vec4(transformed_vertices[2]); /* C---B */
 
             vec3_t vector_ab = vec3_sub(vector_b, vector_a);
             vec3_t vector_ac = vec3_sub(vector_c, vector_a);
@@ -161,7 +166,7 @@ void update(void)
         // Loop all three vertices to perform projection
         for (int j = 0; j < 3; j++) {
             // Project the current vertex
-            projected_vertices[j] = project(transformed_vertices[j]);
+            projected_vertices[j] = project(vec3_from_vec4(transformed_vertices[j]));
 
             // Scale and translate the projected points to the middle of the screen
             projected_vertices[j].x += (window_width / 2);
--- src/matrix.h
+++ src/matrix.h
@@ -1,8 +1,11 @@
 #pragma once
 
+#include "vector.h"
+
 typedef struct {
     float m[4][4];
 } mat4_t;
 
 mat4_t mat4_indentity(void);
 mat4_t mat4_make_scale(float sx, float sy, float sz);
+vec4_t mat4_mul_vec4(mat4_t m, vec4_t v);
